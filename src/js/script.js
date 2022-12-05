9997bea746e3ca54d39eea0a6edcdda23b372a51
Mariia Pinchak <92802080+marypinchak1@users.noreply.github.com>
2022-12-06 01:51:11 +0200

    add-news-section

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,23 @@
 let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
+  $('.content__tabs').slick({
+    slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
-    dots: true,
-    infinite: true
+    dots: false,
+    infinite: true,
+    prevArrow:
+      "<img class='a-left control-c prev arrow-prev' src='../img/slideer-arrow-left.svg'>" ,
+    nextArrow:
+      "<img class='a-right control-c next arrow-next' src='../img/slider-arrow-right.svg'>",
   });
 });
 
+
+$('.item').on('click', function () {
+  $('.item').removeClass('active');
+  $(this).addClass("active");
+  let tabs = $(this).data("tabs");
+  $(".content").addClass("d-none");
+  $(".content[data-tabs = " + tabs + "]").removeClass("d-none");
+  $(".content[data-tabs = " + tabs + "]").addClass("d-block");
+});
