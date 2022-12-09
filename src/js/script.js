3c30be55d1cd33f123d722dddf59a2bc9dbf60ba
Mariia Pinchak <92802080+marypinchak1@users.noreply.github.com>
2022-12-09 16:48:27 +0200

    add-last-changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,38 @@
 let slider = $(document).ready(function () {
-  $('.content__tabs').slick({
+  $(".content__tabs").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
     dots: false,
     infinite: true,
     prevArrow:
-      "<img class='a-left control-c prev arrow-prev' src='../img/slideer-arrow-left.svg'>" ,
+      "<img class='a-left control-c prev arrow-prev' src='../img/slideer-arrow-left.svg'>",
     nextArrow:
       "<img class='a-right control-c next arrow-next' src='../img/slider-arrow-right.svg'>",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          vertical: true,
+          verticalScrolling: true,
+          slidesToShow: 3,
+          speed: 1000,
+          autoplaySpeed: 2500,
+          arrows: false,
+        },
+      },
+    ],
   });
 });
 
-
-$('.item').on('click', function () {
-  $('.item').removeClass('active');
+$(".item").on("click", function () {
+  $(".item").removeClass("active");
   $(this).addClass("active");
   let tabs = $(this).data("tabs");
   $(".content").addClass("d-none");
